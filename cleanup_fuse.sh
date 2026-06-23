95fcb90a34ee800ea67936d240d3f108a78248c7
Marquesha Moore <mooremarquesha@gmail.com>
2026-06-23 19:47:33 -0400

    chore: remove bloat files, tighten gitignore

--- cleanup_fuse.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-count=$(ls .fuse_hidden* 2>/dev/null | wc -l | tr -d ' ')
-if [ "$count" -eq 0 ]; then
-  echo "Nothing to clean up."
-else
-  rm -f .fuse_hidden*
-  echo "Removed $count .fuse_hidden files from $(pwd)"
-fi
